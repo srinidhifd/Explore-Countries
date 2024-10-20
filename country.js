224cf28c9fe38f982a5c7834255be1e2cee76e88
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-10-20 20:19:24 +0530

    added country page and made responsive

--- country.js
+++ country.js
@@ -1,5 +1,6 @@
 const countryName= new URLSearchParams(window.location.search).get('name');
+const flagImage = document.querySelector('.country-details img');
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`).then((res) => res.json()).then((data) => {
-    
+    flagImage.src = data[0].flags.svg;
 });
--- script.js
+++ script.js
@@ -6,7 +6,7 @@ fetch('https://restcountries.com/v3.1/all').then((res) => res.json())
         const countryCard=document.createElement('a');
 countryCard.classList.add('country-card');
 countryCard.href=`country.html?name=${country.name.common}`;
-countryCard.innerHTML=`  <img src="${country.flags.svg}" alt="Flag">
+countryCard.innerHTML=`  <img src="${country.flags.svg}" alt="${country.name.common} Flag">
                     <div class="card-text">
                         <h3 class="card-title">${country.name.common}</h3>
                     <p><strong>Population:</strong> ${country.population.toLocaleString('en-IN')}</p>
