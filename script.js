b48c6e2d74de17a5f578f0fee1fa82b5a4535092
Srinidhi Kulkarni <srinidhi.k2001@gmail.com>
2024-10-20 21:21:35 +0530

    added all files

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-const countriesContainer=document.querySelector('.countries-container');
+const countriesContainer = document.querySelector('.countries-container')
+const filterByRegion = document.querySelector('.filter-by-region')
+const searchInput = document.querySelector('.search-container input')
+const themeChanger = document.querySelector('.theme-changer')
 
-fetch('https://restcountries.com/v3.1/all').then((res) => res.json())
-.then((data) => {
-    data.forEach((country) => {
-        const countryCard=document.createElement('a');
-countryCard.classList.add('country-card');
-countryCard.href=`country.html?name=${country.name.common}`;
-countryCard.innerHTML=`  <img src="${country.flags.svg}" alt="${country.name.common} Flag">
-                    <div class="card-text">
-                        <h3 class="card-title">${country.name.common}</h3>
-                    <p><strong>Population:</strong> ${country.population.toLocaleString('en-IN')}</p>
-                    <p><strong>Region:</strong> ${country.region}</p>
-                    <p><strong>Capital:</strong> ${country.capital}</p>  `;
+let allCountriesData
 
-countriesContainer.appendChild(countryCard);
-    }) 
+fetch('https://restcountries.com/v3.1/all')
+  .then((res) => res.json())
+  .then((data) => {
+    renderCountries(data)
+    allCountriesData = data
+  })
+
+filterByRegion.addEventListener('change', (e) => {
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((res) => res.json())
+    .then(renderCountries)
 })
 
+function renderCountries(data) {
+  countriesContainer.innerHTML = ''
+  data.forEach((country) => {
+    const countryCard = document.createElement('a')
+    countryCard.classList.add('country-card')
+    countryCard.href = `/country.html?name=${country.name.common}`
+    countryCard.innerHTML = `
+          <img src="${country.flags.svg}" alt="${country.name.common} flag" />
+          <div class="card-text">
+              <h3 class="card-title">${country.name.common}</h3>
+              <p><b>Population: </b>${country.population.toLocaleString(
+                'en-IN'
+              )}</p>
+              <p><b>Region: </b>${country.region}</p>
+              <p><b>Capital: </b>${country.capital?.[0]}</p>
+          </div>
+  `
+    countriesContainer.append(countryCard)
+  })
+}
 
 
+searchInput.addEventListener('input',  (e) => {
+  const filteredCountries = allCountriesData.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+  renderCountries(filteredCountries)
+})
+
+themeChanger.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+})
